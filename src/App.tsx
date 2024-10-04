facfc2842d7667ef035f0f6377f0e370d344808d
Deezi <deezitheviper@gmail.com>
2024-10-04 14:46:55 +0100

    gsap .to

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import gsap  from 'gsap'
+import {useGSAP} from '@gsap/react'
+
+
+
+function App() {
+  
+  useGSAP( () => {
+    gsap.to(".card",{x:550, repeat: -1 , yoyo:true, rotation:360, duration:2, ease:'elastic'})
+  }
+  ,[]) 
+
+  return (
+    <>
+<div className='pg'>
+
+  <h3>GSAP.to animation</h3>
+  <p> elements animate from current state to new state</p>
+<div className="card">
+        
+        </div>
+</div>
+    
+    </>
+  )
+}
+
+export default App
